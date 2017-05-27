ad8bdab8bed142ad58873e0cf75adc308b1b6acd
JdRo623 <JdRodriguez623@outlook.com>
2017-05-27 18:55:57 -0500

    Facebook Login

--- app/src/main/java/com/squapp/LoginActivity.java
+++ app/src/main/java/com/squapp/LoginActivity.java
@@ -1,37 +1,51 @@
 package com.squapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
-
-import com.example.jdrod.squapp.R;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.AccessToken;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.squapp.R;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class LoginActivity extends AppCompatActivity {
+    FirebaseAuth.AuthStateListener authListener;
+    FirebaseAuth firebaseAuth;
     LoginButton facebookLoginButton;
     CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
+        firebaseAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_login);
         facebookLoginButton = (LoginButton)findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
-        facebookLoginButton.setReadPermissions("email");
+        facebookLoginButton.setReadPermissions("email", "public_profile");
+        getFireBaseAuth();
         facebookLoginConnection();
     }
     void facebookLoginConnection(){
         facebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
+              //  handleFacebookAccessToken(loginResult.getAccessToken());
                 Toast toast = Toast.makeText(getBaseContext(), "Login",Toast.LENGTH_LONG);
                 Log.w("Login Act","Login");
             }
@@ -48,6 +62,56 @@ public class LoginActivity extends AppCompatActivity {
                 Log.w("Login Act","Error");
             }
         });
+    }
+
+    void getFireBaseAuth(){
+        authListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("AutAct", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("AutAct", "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d("This", "handleFacebookAccessToken:" + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("da", "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("", "signInWithCredential", task.getException());
+                         //   Toast.makeText(FacebookLoginActivity.this, "Authentication failed.",
+                           //       Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (authListener != null) {
+            firebaseAuth.removeAuthStateListener(authListener);
+        }
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
