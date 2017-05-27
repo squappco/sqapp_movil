39413bf312e6670d78a6db23059653e871045ae1
JdRo623 <JdRodriguez623@outlook.com>
2017-05-27 17:20:32 -0500

    Integrating Facebook SDK

--- app/src/main/java/com/example/jdrod/squapp/LoginActivity.java
+++ app/src/main/java/com/example/jdrod/squapp/LoginActivity.java
@@ -2,12 +2,48 @@ package com.example.jdrod.squapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
 
 public class LoginActivity extends AppCompatActivity {
-
+    LoginButton facebookLoginButton;
+    CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        facebookLoginButton = (LoginButton)findViewById(R.id.login_button);
+        facebookLoginButton.setReadPermissions("email");
+        facebookLoginConnection();
+    }
+    void facebookLoginConnection(){
+        facebookLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Toast toast = Toast.makeText(getBaseContext(), "Login",Toast.LENGTH_LONG);
+                Log.d("Login Act","Login");
+            }
+
+            @Override
+            public void onCancel() {
+                Toast toast = Toast.makeText(getBaseContext(), "Cancel",Toast.LENGTH_LONG);
+                Log.d("Login Act","Cancel");
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                Toast toast = Toast.makeText(getBaseContext(), "Error",Toast.LENGTH_LONG);
+                Log.d("Login Act","Error");
+            }
+        });
     }
 }
